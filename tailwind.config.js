947e45f43b740dc7ec96ae9967d57103c2496726
Lars <andrinirinalars2@gmail.com>
2023-11-03 15:59:57 +0300

    Premier commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app-next/pages/**/*.{js,html, jsx, ts, tsx}",
+    "./app-next/components/**/*.{js, ts, jsx, tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
